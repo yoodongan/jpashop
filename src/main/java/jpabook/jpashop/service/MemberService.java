6f7af348223bf4824caf21e8aad41f00583ab34d
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-08-19 13:23:48 +0900

    회원 서비스 개발, @RequiredArgsConstructor 사용해서 자동으로 생성자 만들기

---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,42 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor  // final 붙은 필드 변수만 골라서 생성자를 자동으로 만들어줌.
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    // 회원가입 기능
+    public void join(Member member) {
+        validateDuplicatedMember(member);
+        memberRepository.save(member);
+    }
+
+    private void validateDuplicatedMember(Member member) {
+        List<Member> findMember = memberRepository.findByName(member.getName());
+        if(!findMember.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다!");
+        }
+    }
+
+    // 전체 회원 조회 기능
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    // Id 로 특정 회원 조회
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+
+
+}
+
