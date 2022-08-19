ea18c6f200ebc5f6d8bceed7cfb64792c4667647
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-08-19 15:19:31 +0900

    상품 엔티티 (비즈니스 로직 추가, addStock, removeStock)

--- src/main/java/jpabook/jpashop/domain/Item.java
+++ src/main/java/jpabook/jpashop/domain/Item.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -25,4 +26,17 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    public void addStock(int quantity) {
+        quantity += quantity;
+    }
+    public void removeStock(int quantity) {
+        int restStock = stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("재고가 부족합니다!");
+        }
+        stockQuantity = restStock;
+    }
+
+
 }
