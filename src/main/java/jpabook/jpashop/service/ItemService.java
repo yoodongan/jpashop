7fd443c479863e857c2f67791f4db51e00497c51
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-08-19 15:34:09 +0900

    ItemService 추가 (상품 서비스)

---
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+    // saveItem, findItems, findOne
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long memberId) {
+        return itemRepository.findOne(memberId);
+    }
+
+
+
+
+}
