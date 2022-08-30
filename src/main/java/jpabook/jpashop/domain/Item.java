b5caf3ea6608bb39818f9b3ec62f101cf8541105
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-08-30 11:30:00 +0900

    상품 엔티티, 상품 리포지터리 개발

--- src/main/java/jpabook/jpashop/domain/Item.java
+++ src/main/java/jpabook/jpashop/domain/Item.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -25,4 +26,18 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    /* 비즈니스 로직 시작 */
+    // addStock, removeStock(예외 처리까지)
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+    public void removeStock(int quantity) {
+        int restStock = stockQuantity-quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("재고가 부족합니다!");
+        }
+        stockQuantity -= quantity;
+    }
+
 }
